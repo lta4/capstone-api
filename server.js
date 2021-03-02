651f62a290af516ada686da3149b5176dddcc839
Andrew <andrew.mk4@yahoo.com>
2021-03-02 13:10:46 -0800

    update

--- server.js
+++ server.js
@@ -141,9 +141,11 @@ const root = {
 };
 // Create an express server and a GraphQL endpoint
 const app = express();
-app.use('/graphql', graphqlHTTP({
+app.use('/graphql' , graphqlHTTP({
     schema: schema,
     rootValue: root,
     graphiql: true
 }));
-app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
+
+const port = process.env.PORT || "4000";
+app.listen(port, () => console.log(`listening on: http://localhost:${port}/graphql`));
